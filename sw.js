504e7c847fa97136d9e1fedcfdfcb6bdfb25ddf3
Yujaidy <karg327@gmail.com>
2022-06-24 19:56:43 -0400

    update

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@ const STATIC_CACHE='static-v1';
 const DYNAMYC_CACHE='dinamyc-v1';
 const INMUTABLE_CACHE='inmutable-v1';
 const APP_SHELL=[
-    // '/',
+    // '/', 
     'index.html',
     'css/style.css',
     'img/favicon.ico',
