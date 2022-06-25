2aa495da6a3e066bbfda574415673dd1bc5789ab
Yujaidy <karg327@gmail.com>
2022-06-24 20:52:08 -0400

    mejorando la vista

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 importScripts('js/sw-utils.js');
 const STATIC_CACHE='static-v1';
-const DYNAMYC_CACHE='dinamyc-v1';
+const DYNAMIC_CACHE= 'dynamic-v2';
 const INMUTABLE_CACHE='inmutable-v1';
 const APP_SHELL=[
     // '/', 
@@ -25,15 +25,18 @@ self.addEventListener('install',e=>{
     const cacheInmutable = caches.open(INMUTABLE_CACHE).then(cache=>cache.addAll(APP_SHELL_INMUTABLE));
     e.waitUntil(Promise.all([cacheStatic, cacheInmutable]));
 });
-self.addEventListener('activate',e=>{
-    const respuesta = caches.keys().then(keys=>{
-        keys.forEach(key=>{
-            if(key !== STATIC_CACHE && key.includes('static')){
+self.addEventListener('activate', e => {
+    const respuesta = caches.keys().then( keys => {
+        keys.forEach( key => {
+            if (  key !== STATIC_CACHE && key.includes('static') ) {
+                return caches.delete(key);
+            }
+            if (  key !== DYNAMIC_CACHE && key.includes('dynamic') ) {
                 return caches.delete(key);
             }
         });
     });
-    e.waitUntil(respuesta);
+    e.waitUntil( respuesta );
 });
 self.addEventListener('fetch', e=>{
     const respuesta= caches.match(e.request).then(res=>{
