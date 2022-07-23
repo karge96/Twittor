eaaf0c037c1009aacf6984ec28945583d3fa3df0
Yujaidy <karg327@gmail.com>
2022-07-23 12:40:05 -0400

    actializacion

--- js/app.js
+++ js/app.js
@@ -8,7 +8,6 @@ if(navigator.serviceWorker){
     navigator.serviceWorker.register(swLocation);
 }
 // Referencias de jQuery
-
 var titulo      = $('#titulo');
 var nuevoBtn    = $('#nuevo-btn');
 var salirBtn    = $('#salir-btn');
@@ -16,22 +15,14 @@ var cancelarBtn = $('#cancel-btn');
 var postBtn     = $('#post-btn');
 var avatarSel   = $('#seleccion');
 var timeline    = $('#timeline');
-
 var modal       = $('#modal');
 var modalAvatar = $('#modal-avatar');
 var avatarBtns  = $('.seleccion-avatar');
 var txtMensaje  = $('#txtMensaje');
-
 // El usuario, contiene el ID del héroe seleccionado
 var usuario;
-
-
-
-
 // ===== Codigo de la aplicación
-
 function crearMensajeHTML(mensaje, personaje) {
-
     var content =`
     <li class="animated fadeIn fast">
         <div class="avatar">
@@ -43,22 +34,15 @@ function crearMensajeHTML(mensaje, personaje) {
                 <br/>
                 ${ mensaje }
             </div>
-            
             <div class="arrow"></div>
         </div>
     </li>
     `;
-
     timeline.prepend(content);
     cancelarBtn.click();
-
 }
-
-
-
 // Globals
 function logIn( ingreso ) {
-
     if ( ingreso ) {
         nuevoBtn.removeClass('oculto');
         salirBtn.removeClass('oculto');
@@ -70,43 +54,27 @@ function logIn( ingreso ) {
         salirBtn.addClass('oculto');
         timeline.addClass('oculto');
         avatarSel.removeClass('oculto');
-
         titulo.text('Seleccione Personaje');
-    
     }
-
 }
-
-
 // Seleccion de personaje
 avatarBtns.on('click', function() {
-
     usuario = $(this).data('user');
-
     titulo.text('@' + usuario);
-
     logIn(true);
-
 });
-
 // Boton de salir
 salirBtn.on('click', function() {
-
     logIn(false);
-
 });
-
 // Boton de nuevo mensaje
 nuevoBtn.on('click', function() {
-
     modal.removeClass('oculto');
     modal.animate({ 
         marginTop: '-=1000px',
         opacity: 1
     }, 200 );
-
 });
-
 // Boton de cancelar mensaje
 cancelarBtn.on('click', function() {
    modal.animate({ 
@@ -117,7 +85,6 @@ cancelarBtn.on('click', function() {
         txtMensaje.val('');
     });
 });
-
 // Boton de enviar mensaje
 postBtn.on('click', function() {
 
@@ -126,7 +93,5 @@ postBtn.on('click', function() {
         cancelarBtn.click();
         return;
     }
-
     crearMensajeHTML( mensaje, usuario );
-
 });
